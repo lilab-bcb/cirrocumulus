967fa9192d781fb682ce11ee5397c701faae3519
jgould <jgould@broadinstitute.org>
2019-10-16 14:40:54 -0400

    natsort

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-requires = ["ujson", "flask", "anndata", "fsspec", "flask-compress"]
+requires = ["ujson", "flask", "anndata", "fsspec", "flask-compress", "natsort"]
 
 setup(
     version='0.0.8',
