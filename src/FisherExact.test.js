1e837afa5ba8547f721fbda4e412511755b3ac2f
jgould <jgould@broadinstitute.org>
2021-04-06 10:11:25 -0400

    added

---
+++ src/FisherExact.test.js
@@ -0,0 +1,29 @@
+// R chisq.test
+
+const {fisherTest} = require('./FisherExact');
+
+
+it('test_1', () => {
+    /*
+   TeaTasting <-
+matrix(c(3, 1, 1, 3),
+       nrow = 2,
+       dimnames = list(Guess = c("Milk", "Tea"),
+                       Truth = c("Milk", "Tea")))
+fisher.test(TeaTasting)
+     */
+    const result = fisherTest(3, 1, 1, 3);
+    expect(result).toBeCloseTo(0.4857);
+});
+
+
+it('test_2', () => {
+    /*
+ > Convictions <- matrix(c(2, 10, 15, 3), nrow = 2,
+    +                       dimnames =
+        +                           list(c("Dizygotic", "Monozygotic"),
+            +                                c("Convicted", "Not convicted")))
+     */
+    const result = fisherTest(2, 15, 10, 3);
+    expect(result).toBeCloseTo(0.0005367);
+});
