72715e45acf28996e7483fea1c47637b318ae423
Joshua Gould <jgould@cellarity.com>
2021-06-24 16:19:29 -0400

    debug CI

--- e2e/jest-puppeteer.config.js
+++ e2e/jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     server: {
-        command: "cirro launch ../test-data/pbmc3k_no_raw.h5ad --no-open --host 0.0.0.0",
+        command: "cirro launch ../test-data/pbmc3k_no_raw.h5ad --no-open --host localhost",
         port: 5000,
         launchTimeout: 10000
     }
