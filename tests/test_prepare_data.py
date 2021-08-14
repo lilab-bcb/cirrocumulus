19e2d024966c76cecb10717a56dfaab75f1025f7
Joshua Gould <jgould@cellarity.com>
2021-08-13 21:33:35 -0400

    pass list of datasets to prepare data

--- tests/test_prepare_data.py
+++ tests/test_prepare_data.py
@@ -54,6 +54,6 @@ def test_prepare_parquet(test_data, measures, dimensions, continuous_obs, basis,
     output_dir = str(tmp_path)
     test_data = test_data[:, measures]
     test_data.obs = test_data.obs[dimensions + continuous_obs]
-    prepare_data = PrepareData(adata=test_data, output=output_dir)
+    prepare_data = PrepareData(datasets=[test_data], output=output_dir)
     prepare_data.execute()
     read_and_diff(ParquetDataset(), output_dir, test_data, measures, dimensions, continuous_obs, basis)
