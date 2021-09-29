f1356d4bc99a0b3561f4a304f41fcee301257d6e
Joshua Gould <jgould@cellarity.com>
2021-09-29 18:07:01 -0400

    use natsort

--- src/DatasetSelector.js
+++ src/DatasetSelector.js
@@ -25,40 +25,11 @@ import ReactMarkdown from 'markdown-to-jsx';
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {OPEN_DATASET_DIALOG, setDialog} from './actions';
-import {REACT_MD_OVERRIDES} from './util';
+import {NATSORT, REACT_MD_OVERRIDES} from './util';
 import {intFormat} from './formatters';
 import {visuallyHidden} from '@mui/utils';
 import Box from '@mui/material/Box';
 
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
 
 export function DatasetSelector(props) {
     const [searchText, setSearchText] = useState('');
@@ -136,9 +107,15 @@ export function DatasetSelector(props) {
         filteredChoices = filteredChoices.filter(choice => choice.name.toLowerCase().indexOf(searchTextLower) !== -1 ||
             (choice.description != null && choice.description.toLowerCase().indexOf(searchTextLower) !== -1));
     }
-    filteredChoices = stableSort(filteredChoices, getComparator(order, orderBy));
+    if (orderBy != null) {
+        filteredChoices.sort((item1, item2) => {
+            return NATSORT(item1[orderBy], item2[orderBy]);
+        });
+        if (order == 'desc') {
+            filteredChoices.reverse();
+        }
+    }
     const datasetDetailsOpen = Boolean(datasetDetailsEl);
-    const hasMoreInfo = selectedDataset && (selectedDataset.title || selectedDataset.description);
     // const group2Items = groupBy(filteredChoices, item => item[datasetSelectorGroupBy] || '');
     // const groups = Object.keys(group2Items);
     // groups.sort(NATSORT);
