d0341dfb571a615c886d7069493c8a87a10fe8d9
Joshua Gould <jgould@cellarity.com>
2021-10-15 10:48:42 -0400

    test that precomputed de results can be loaded

---
+++ e2e/tests/de_results.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+const gm = require('gm');
+
+
+it('de_results"', async () => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.setViewport({width: 1500, height: 1000});
+    await page.goto('http://127.0.0.1:5000#q={"dataset":"../test-data/pbmc3k_no_raw.h5ad", "jobId":"cirro-rank_genes_groups-0"}');
+    await page.click('[data-testid="results-tab"]');
+    await page.waitForSelector('[data-testid="dot-plot-table"]');
+    await browser.close();
+});
+
+
+
