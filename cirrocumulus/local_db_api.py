0b229002fac4c42cb9e434ddf26b638234686995
Joshua Gould <jgould@cellarity.com>
2021-11-12 16:47:18 -0500

    catch all json errors

--- cirrocumulus/local_db_api.py
+++ cirrocumulus/local_db_api.py
@@ -64,7 +64,7 @@ class LocalDbAPI(AbstractDB):
                 with open(json_path, 'rt') as f:
                     try:
                         json_data.update(json.load(f))
-                    except json.decoder.JSONDecodeError:
+                    except:
                         print('Unable to load {}'.format(json_path))
             meta = create_dataset_meta(path)
             if 'filters' not in json_data:
