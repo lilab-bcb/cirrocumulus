28bdb247a3d8eccf8997c14f24616ecd2a6c84ce
Joshua Gould <jgould@cellarity.com>
2021-12-20 11:56:39 -0500

    version updates

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     preset: "jest-puppeteer",
     testMatch: ["**/tests/*.js"],
-    transform: {
-        "\\.js$": "react-scripts/config/jest/babelTransform"
-    },
     testTimeout: 10000
 };
