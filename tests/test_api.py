da418e66f66c9a4760d48e0ff968c211b2c478c2
Joshua Gould <jgould@cellarity.com>
2022-01-13 07:45:52 -0500

    added additional test fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,23 +1,44 @@
+import os
+
+import anndata
 import pytest
-
+from cirrocumulus.envir import CIRRO_DB_URI
 from cirrocumulus.launch import create_app, configure_app
+from cirrocumulus.prepare_data import PrepareData
+from cirrocumulus.serve import cached_app, DEFAULT_DB_URI
 
 
-@pytest.fixture
-def client():
-    app = create_app()
-    configure_app(app, ['test-data/pbmc3k_no_raw.h5ad'], None, None)
+@pytest.fixture(scope='session', params=[True, False])
+def app_conf(request, tmpdir_factory):
+    dataset_path = 'test-data/pbmc3k_no_raw.h5ad'
+
+    if not request.param:
+        app = create_app()
+        configure_app(app, [dataset_path], None, None)
+        dataset_id = dataset_path
+    else:
+        os.environ[CIRRO_DB_URI] = DEFAULT_DB_URI
+        app = cached_app()
     with app.test_client() as client:
-        yield client
+        if request.param:
+            # insert dataset
+            output_dir = str(tmpdir_factory.mktemp("data").join("test.zarr"))
+            PrepareData(datasets=[anndata.read(dataset_path)], output=output_dir, output_format='zarr',
+                        no_auto_groups=True).execute()
+            r = client.post('/api/dataset', data=dict(url=output_dir, name='test'))
+            dataset_id = r.json['id']
+        yield client, dataset_id
 
 
-def test_server(client):
+def test_server(app_conf):
+    client, dataset_id = app_conf
     r = client.get('/api/server').json
     assert r['email'] is None
 
 
-def test_schema(client):
-    r = client.get('/api/schema?id=test-data/pbmc3k_no_raw.h5ad').json
+def test_schema(app_conf):
+    client, dataset_id = app_conf
+    r = client.get('/api/schema?id={}'.format(dataset_id)).json
     assert isinstance(r['var'], list)
     assert isinstance(r['obs'], list)
     assert isinstance(r['embeddings'], list)
